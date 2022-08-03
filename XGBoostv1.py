f9799fa8e21767c14f2fe6868c31cfcd780bc498
Laxman B <ed18b006@smail.iitm.ac.in>
2022-08-04 01:42:20 +0530

    Very Basic version almost completed
    XGBoost very basic version done, but need to analyse parameter in order to decided what parameters to be taken for hyper-parameter tuning

--- XGBoostv1.py
+++ XGBoostv1.py
@@ -1,19 +1,17 @@
 from datagen import X_train,X_test,Y_train,Y_test
-import xgboost as xgb
+from xgboost import XGBRegressor
 from Regressionscorev1 import regressionscorev1
 import numpy as np
 import matplotlib.pyplot as plt
-
-# _____BRINGING THE DATASET INTO XGBOOST D MATRIX FORMAT______
-
-
-
-
 
 # _______SUPPORT VECTOR REGRESSION__________________
 
 
 
+boost = XGBRegressor()
+boost.fit(X_train,Y_train)
+ytest_predict = boost.predict(X_test)
+
 # _______REGRESSION SCORE USING SVR__________________
 
-regressionscorev1(Y_test,ytest_predict,'SVR')
+regressionscorev1(Y_test,ytest_predict,'XGBoost')
